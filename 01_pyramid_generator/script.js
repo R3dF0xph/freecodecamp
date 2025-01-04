797b3590b2e3e30a89991fd396e070ecfa9e35ba
r3df0x <ip.xb.itsandbox@gmail.com>
2025-01-05 02:04:50 +0300

    done step 1-16 of pyramid generator freecode

---
+++ 01_pyramid_generator/script.js
@@ -0,0 +1,23 @@
+//A pyramid generator is a program where you can set the type of character, 
+//the count for the pyramid, and the direction of the pyramid. 
+//The program will then generate a pyramid based on those inputs.
+
+// step 1-12
+let character = "Hello";
+console.log(character);
+character = "World";
+console.log(character);
+let secondCharacter;
+secondCharacter = character;
+console.log(secondCharacter);
+
+// step 13
+let profession = "teacher";
+let age;
+console.log(profession);
+console.log(age);
+
+//step 14
+let count = 8;
+console.log(count + 1);
+

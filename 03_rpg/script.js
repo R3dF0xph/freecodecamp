52c809ad2e6b1797680437803454cf5da1eddc07
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-01-22 12:16:12 +0300

    step 48 add cat

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -22,12 +22,14 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [];
+const cat = {};
+console.log(cat);
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 function update(location) {
-    
+
 }
 function goTown() {
     button1.innerText = "Go to store";

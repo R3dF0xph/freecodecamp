922b5b746af4719b17cc1697350c4cea451acc3a
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-01-22 17:39:23 +0300

    fix bug {

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -50,7 +50,7 @@ button3.onclick = fightDragon;
 function update(location) {
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
-    button3.innerText = location{"button text"}[2];
+    button3.innerText = location["button text"][2];
     button1.onclick = location["button functions"][0];
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];

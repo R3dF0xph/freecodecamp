cd118f7e6cade1bc56e79c0443ea6487db6a3eb7
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-01-23 16:20:32 +0300

    step 77 add if to check gold

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -81,10 +81,12 @@ function fightDragon() {
 }
 
 function buyHealth() {
+    if(gold >= 10) {
     gold -= 10;
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
+    }
 }
 
 function buyWeapon() {

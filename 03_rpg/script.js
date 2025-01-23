e7d8eb01365a167dc2b07b3bae80201f4a64b1cf
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-01-23 15:23:25 +0300

    step 72 add third locations and update goCave

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -33,6 +33,12 @@ const locations = [
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)","Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
 ];
 /*const cat = {
@@ -66,7 +72,8 @@ function goStore() {
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    //console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {

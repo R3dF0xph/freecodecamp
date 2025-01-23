24ad52cf7a82df5294146993c67b5b4133b46fb3
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-01-23 15:31:26 +0300

    step 74 update buyHealth

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -81,7 +81,10 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
 }
 
 function buyWeapon() {

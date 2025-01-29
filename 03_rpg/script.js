bf133d720c9e28d179d86c273ea81b3f1637dbd0
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-01-29 11:53:43 +0300

    step 107 update innerText for sell

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -132,8 +132,9 @@ function sellWeapon() {
     if (inventory.length > 1) {
         gold += 15;
         goldText.innerText = gold;
-        let currentWeapon;
-    }
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+    }   
 }
 function fightSlime() {
 

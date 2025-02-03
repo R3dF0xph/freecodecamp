a17fb57219c036e70097c4f49109a8036b2a8114
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-02-03 08:17:59 +0300

    step 134 add loctions kill monster

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -80,6 +80,12 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: "The monster screams Arg! as it dies. You gain expireience points and find gold."
     }
 ];
 /*const cat = {

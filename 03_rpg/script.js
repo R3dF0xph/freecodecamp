505d7506c419bc36ddb80d806e1ea4cd4b10b201
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-02-05 08:24:19 +0300

    step 149 ternary operator

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -223,7 +223,23 @@ function attack() {
 function getMonsterAttackValue(level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
     console.log(hit);
-    return hit;
+    /*using a ternary operator to ensure negative values are not returned.
+
+The ternary operator is a conditional operator and can be used as a one-line if-else statement. The syntax is: condition ? expressionIfTrue : expressionIfFalse.
+
+Here is an example of returning a value using an if-else statement and a refactored example using a ternary operator:
+
+Example Code
+// if-else statement
+if (score > 0) {
+  return score
+} else {
+  return default_score
+
+// ternary operator
+return score > 0 ? score : default_score
+*/
+    return hit > 0 ? hit : 0;
 }
 
 function dodge() {

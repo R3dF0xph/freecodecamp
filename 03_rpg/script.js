3fd6d06f1047d6817bd164ef60f2d1b2add36e14
R3dF0xph <ip.xb.itsandbox@gmail.com>
2025-02-10 08:25:29 +0300

    step 169 add if includes guess in numbers

--- 03_rpg/script.js
+++ 03_rpg/script.js
@@ -305,6 +305,9 @@ function pick(guess) {
     for (let i = 0; i < 10; i++) {
         text.innerText += numbers[i] + "\n";
     }
+    if (numbers.includes(guess)) {
+        
+    }
 }
 
 function pickTwo() {
